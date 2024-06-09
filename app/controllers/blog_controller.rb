6024cf5f11fdf7ab76b236ed0847ce025aab6a60
Anh Nguyen <anh.nt@josys.com>
2024-06-09 13:40:58 +0700

    Before add bun

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,20 +1,5 @@
 class BlogController < ApplicationController
-  def new
-    @blog = Blog.new
-  end
-
   def index
     @blogs = Blog.all.limit(10)
-  end
-
-  def create
-    @blog = Blog.new(params[:blog])
-    if @blog.save
-      redirect_to @blog
-    else
-      # This line overrides the default rendering behavior, which
-      # would have been to render the "create" view.
-      render "new"
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  namespace :admin do
+    resources :blog, only: %i[index new]
+  end
 end
