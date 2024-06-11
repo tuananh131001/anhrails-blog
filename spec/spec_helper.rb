b9f7e3cb5b88b102d60ba2b3fb56dae811e97175
Anh Nguyen <anh.nt@josys.com>
2024-06-11 17:42:56 +0700

    add precompile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,11 +91,4 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
-  # https://github.com/rails/tailwindcss-rails/issues/153
-  config.before(:suite) do
-    if config.files_to_run.any? { |path| path.start_with?(Rails.root.join("spec/system").to_s) }
-      Rails.application.load_tasks
-      Rake::Task["tailwindcss:build"].invoke
-    end
-  end
 end
