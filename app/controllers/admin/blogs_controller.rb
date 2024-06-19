c8576075ebadc619dc2e35c265837ac0a16c69c5
Anh Nguyen <anh.nt@josys.com>
2024-06-19 15:42:29 +0700

    Add header and optimise controller

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -2,15 +2,14 @@
 
 module Admin
   class BlogsController < ApplicationController
+    before_action :set_blog, only: %i[show edit update destroy]
     before_action :set_light_theme, only: %i[new create edit]
 
     def index
-      @blogs = Blog.limit(10)
+      @blogs = Blog.all.limit(10) # Use `all` for efficiency if no filtering needed
     end
 
-    def show
-      @blog = Blog.find(params[:id])
-    end
+    def show; end
 
     def new
       @blog = Blog.new
@@ -19,32 +18,38 @@ module Admin
     def create
       @blog = Blog.new(blog_params)
       if @blog.save
-        redirect_to [:admin, @blog], notice: 'Blog was successfully created.'
+        redirect_to [:admin, @blog], notice: 'Blog successfully created.'
       else
-        flash.now[:alert] = 'Could not save blog'
+        flash.now[:alert] = 'Blog creation failed.'
         render :new, status: :unprocessable_entity
       end
     end
 
     def update
       if @blog.update(blog_params)
-        redirect_to blogs_path, notice: 'Blog was successfully updated.'
+        redirect_to admin_blogs_path, notice: 'Blog successfully updated.'
       else
         render :edit, status: :unprocessable_entity
       end
     end
 
-    def edit
-      @blog = Blog.find(params[:id])
-    end
+    def edit; end
 
     def destroy
-      @blog = Blog.find(params[:id])
-      flash.now[:notice] = 'You has succesfully deleted the blog'
       @blog.destroy
+      flash[:notice] = 'Blog successfully deleted.'
+      redirect_to admin_blogs_path
     end
 
     private
+
+    def set_blog
+      @blog = Blog.find_by(id: params[:id])
+      unless @blog
+        flash.now[:alert] = 'Cannot find the blog.'
+        redirect_to admin_blogs_path
+      end
+    end
 
     def blog_params
       params.require(:blog).permit(:title, :content, :tag)
