0a831849fa4bba6fa7d754db66b0ca5cec7aaf23
Anh Nguyen <anh.nt@josys.com>
2024-06-19 15:20:00 +0700

    Remove editorjs

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,10 +7,5 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin '@editorjs/editorjs', to: '@editorjs--editorjs.js' # @2.29.0
-pin '@editorjs/code', to: '@editorjs--code.js' # @2.9.0
-pin '@editorjs/header', to: '@editorjs--header.js' # @2.8.1
-pin '@editorjs/list', to: '@editorjs--list.js' # @1.9.0
-pin '@editorjs/paragraph', to: '@editorjs--paragraph.js' # @2.11.4
 pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.esm.js'
